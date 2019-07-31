4aa717a5791acd49985da94d4180b53fd7802d73
Michael Claborn <mclaborn2010@gmail.com>
2019-07-31 14:03:18 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,101 @@
+const choices = document.querySelectorAll(".choice");
+const score = document.getElementById("score");
+const result = document.getElementById("result");
+const restart = document.getElementById("restart");
+const modal = document.querySelector(".modal");
+const scoreboard = {
+  player: 0,
+  computer: 0
+};
+
+// Play Function
+function play(e) {
+  restart.style.display = "inline-block";
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  showWinner(winner, computerChoice);
+  console.log(playerChoice, computerChoice, winner);
+}
+
+// Get computer choice
+function getComputerChoice() {
+  const rand = Math.random();
+  if (rand < 0.34) {
+    return "rock";
+  } else if (rand <= 0.67) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+// Get Game Winner
+function getWinner(p, c) {
+  if (p === c) {
+    return "draw";
+  } else if (p === "rock") {
+    if (c === "paper") {
+      return "computer";
+    } else {
+      return "player";
+    }
+  } else if (p === "paper") {
+    if (c === "scissors") {
+      return "computer";
+    } else {
+      return "player";
+    }
+  } else if (p === "scissors") {
+    if (c === "rock") {
+      return "computer";
+    } else {
+      return "player";
+    }
+  }
+}
+
+function showWinner(winner, computerChoice) {
+  if (winner === "player") {
+    // Inc player score
+    scoreboard.player++;
+    // Show modal result
+    result.innerHTML = `
+        <h1 class="text-win">You Win</h1>
+        <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+        <p>Computer chose <strong></strong></p>
+        `;
+  } else if (winner === "computer") {
+    // Inc computer score
+    scoreboard.computer++;
+    // Show modal result
+    result.innerHTML = `
+        <h1 class="text-lose">You Lose</h1>
+        <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+        <p>Computer chose <strong></strong></p>
+        `;
+  } else {
+    result.innerHTML = `
+    <h1>It's a Draw</h1>
+    <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+    <p>Computer chose <strong></strong></p>
+    `;
+  }
+
+  // Show score
+  score.innerHTML = `
+  <p>Player: ${scoreboard.player}</p>
+  <p>Computer: ${scoreboard.computer}</p>
+  `;
+  modal.style.display = "block";
+}
+// Clear modal
+function clearModal(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Event Listeners
+choices.forEach(choice => choice.addEventListener("click", play));
+window.addEventListener("click", clearModal);
